ca812e9ff8c111649d7b8ad65a7d3d6900875435
FloodcatNet <ololo43@yandex.ru>
2011-08-26 21:11:03 +0600

    first version

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,6 @@
+class HomeController < ApplicationController
+  def index
+    puts "ololo123"
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testing::Application.config.secret_token = '971f9a4fed4a929ad4ef748332bf2f41c030cbb2f0cf17f260829ea9e6a538f2654f8af7c95b071bcf16e262173a0f59a0a81d64640d4813b67dab4d11bed449'
